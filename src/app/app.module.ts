d444bc4e930d4135a789cc1dcfdd52dca1e53969
codeGuyLeo <leo_garnica_estrella@hotmail.com>
2022-11-23 17:09:58 +0100

    aggiornato il form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { LandingPageComponent } from './pages/landing-page/landing-page.component';
 import {RouterModule} from "@angular/router";
@@ -10,7 +9,9 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { ContactComponent } from './pages/contact/contact.component';
 import { ProductsComponent } from './pages/products/products.component';
 import { FooterComponent } from './footer/footer.component';
-
+import { ReactiveFormsModule } from '@angular/forms';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations'
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
 
 
 
@@ -28,6 +29,9 @@ import { FooterComponent } from './footer/footer.component';
         BrowserModule,
         RouterModule,
         AppRoutingModule,
+        ReactiveFormsModule,
+        NoopAnimationsModule,
+        MatAutocompleteModule
     ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
@@ -7,9 +8,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
-  constructor() { }
+  contactForm: FormGroup;
+  options: string[] = [];
 
+  constructor() {
+    this.contactForm = new FormGroup({
+      firstName: new FormControl("", [Validators.required]),
+      lastName: new FormControl("",[Validators.required]),
+      email: new FormControl("", [Validators.required, Validators.email]),
+      message: new FormControl("", [Validators.minLength(16)])
+    });
+
+    this.contactForm.get('email')?.valueChanges.subscribe((value: string) => {
+        if (value.includes('@')) {
+          this.options = [`${value}yahoo.com`,`${value}yahoo.it`,`${value}yahoo.org`, `${value}gmail.it`, `${value}gmail.org`, `${value}gmail.us`, `${value}gmail.com`, `${value}outlook.it`, `${value}outlook.org`, `${value}outlook.us`, `${value}outlook.com`, `${value}hotmail.it`, `${value}hotmail.org`, `${value}hotmail.us`, `${value}hotmail.com`];
+
+        } else {
+          this.options = [`${value}@yahoo.com`,`${value}@yahoo.it`,`${value}@yahoo.org`, `${value}@gmail.it`, `${value}@gmail.org`, `${value}@gmail.us`, `${value}@gmail.com`, `${value}@outlook.it`, `${value}@outlook.org`, `${value}@outlook.us`, `${value}@outlook.com`, `${value}@hotmail.it`, `${value}@hotmail.org`, `${value}@hotmail.us`, `${value}@hotmail.com`];
+
+        }
+      })
+   }
+   
+
+   get firstName() {
+    return this.contactForm.get('firstName')
+   }
+
+   get lastName() {
+    return this.contactForm.get('lastName')
+   }
+   
+   get message() {
+    return this.contactForm.get('message')
+   }
   ngOnInit(): void {
+
   }
 
 }
