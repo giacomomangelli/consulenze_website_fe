64bfb4d70aca72b089da1d5e3eccca4bcf9b4231
GIACOMO_PC\giaco <giacomomsidp@gmail.com>
2022-12-30 01:07:19 +0100

    navbar modified

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, HostListener, OnInit} from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -8,9 +8,17 @@ import { Router } from '@angular/router';
 })
 export class NavbarComponent implements OnInit {
 
-
   constructor(private router: Router) { }
 
+  @HostListener('window:scroll', ['$event'])
+  onScroll(): void {
+    let element = document.querySelector('#nav') as HTMLElement;
+    if (window.scrollY > element.clientHeight*2) {
+      element.classList.add('bg-black');
+    } else {
+      element.classList.remove('bg-black');
+    }
+  }
   ngOnInit(): void {
   }
 
